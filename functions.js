d12ea416141fc618784da7314d931810b8cd05b6
torrosanttu <santtu.torro@gmail.com>
2025-04-04 03:45:02 +0300

    Javascriptiä ja indexiä muokattu niin, että saadaan laskettua jo työtuntien pituus.

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+var savedHours = localStorage.getItem('bankHours') ? parseFloat(localStorage.getItem('bankHours')) : 0;
+
 function calculateHours() {
 var startingtimeInput = document.getElementById('startingtime').value;
 var endingtimeInput = document.getElementById('endingtime').value;
@@ -10,5 +12,20 @@ var endingMinutes = parseFloat(endingtimeInput.split(":")[1]);
 
 var workhours = endingHours - startingHours;
 var workminutes = endingMinutes - startingMinutes;
+
+if (workminutes < 0) {
+    workminutes += 60;
+    workhours -=1;
+}
+
+if (workhours > 8) {
+    savedHours += workhours - 8 + workminutes / 60;
+    localStorage.setItem('bankHours', savedHours.toFixed(2)) //pyöristetään kaksi desimaalia
+    workhours = 8;
+}
+
+document.getElementById('hours').textContent = workhours + " Hours and " + workminutes + " minutes "; //syöttää tuloksen indexiin.
+
+updateLocalStorageValues();
 
 }
